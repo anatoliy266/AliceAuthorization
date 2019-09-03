04d42431ae05e3a9665fc9c90bc3f72509abfaf9
Stajer Prog <prog@stranzit.ru>
2019-09-03 09:01:06 +0800

    123

---
+++ WebApplication1/Models/AuthModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class AuthModel
+    {
+        public string State { get; set; }
+        public string RedirectUri { get; set; }
+        public string ResponseType { get; set; }
+        public string ClientId { get; set; }
+        public string Scope { get; set; }
+        public string Code { get; set; }
+    }
+
+    ///response_type=code&client_id=123
+}
